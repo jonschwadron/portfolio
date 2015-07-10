0db267607f9a6b6e9a78093c43a45109c52d43f1
jonschwadron <jonschwadron@gmail.com>
2015-07-10 20:23:20 +0000

    fixed urls

--- magic/urls.py
+++ magic/urls.py
@@ -3,4 +3,21 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.magicpage),
+    url(r'^index.html', views.index, name="index"),
+    url(r'^messages.html', views.messages, name="messages"),
+    url(r'^tasks.html', views.tasks, name="tasks"),
+    url(r'^ui.html', views.ui, name="ui"),
+    url(r'^widgets.html', views.widgets, name="widgets"),
+    url(r'^submenu.html', views.submenu, name="submenu"),
+    url(r'^submenu2.html', views.submenu2, name="submenu2"),
+    url(r'^submenu3.html', views.submenu3, name="submenu3"),
+    url(r'^form.html', views.form, name="form"),
+    url(r'^chart.html', views.chart, name="chart"),
+    url(r'^typography.html', views.typography, name="typography"),
+    url(r'^gallery.html', views.gallery, name="gallery"),
+    url(r'^table.html', views.table, name="table"),
+    url(r'^calendar.html', views.calendar, name="calendar"),
+    url(r'^file-manager.html', views.filemanager, name="filemanager"),
+    url(r'^icon.html', views.icon, name="icon"),
+    url(r'^login.html', views.login, name="login"),
 ]
--- magic/views.py
+++ magic/views.py
@@ -2,3 +2,54 @@ from django.shortcuts import render
 
 def magicpage(request):
     return render(request, 'index.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def messages(request):
+    return render(request, 'messages.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def ui(request):
+    return render(request, 'ui.html')
+
+def widgets(request):
+    return render(request, 'widgets.html')
+
+def submenu(request):
+    return render(request, 'submenu.html')
+
+def submenu2(request):
+    return render(request, 'submenu2.html')
+
+def submenu3(request):
+    return render(request, 'submenu3.html')
+
+def form(request):
+    return render(request, 'form.html')
+
+def chart(request):
+    return render(request, 'chart.html')
+
+def typography(request):
+    return render(request, 'typography.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def table(request):
+    return render(request, 'table.html')
+
+def calendar(request):
+    return render(request, 'calendar.html')
+
+def filemanager(request):
+    return render(request, 'file-manager.html')
+
+def icon(request):
+    return render(request, 'icon.html')
+
+def login(request):
+    return render(request, 'login.html')
