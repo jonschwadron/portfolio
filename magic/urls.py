6b795bb68df9c50a1aab0cf84cbecf4bbf64678b
jonschwadron <jonschwadron@gmail.com>
2015-09-08 19:16:39 +0000

    udpate

--- magic/urls.py
+++ magic/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage),
+    url(r'^about/$', views.about),
 
     url(r'^personal/nyc-calc/$', views.nyc_calc, name="nyc-calc"),
     url(r'^personal/suggestion-box/$', views.suggestion_box, name="sugestion-box"),
